a927372ce0b4cfefbf75e464e5f6e488aaa19376
arcanjotiago <tiagoarcanjom@gmail.com>
2024-12-31 21:16:27 -0300

    code refactoring

--- README.md
+++ README.md
@@ -10,3 +10,42 @@ POST /calcnumber - Return the calc the previous number and next number,
 
 POST /validatemail - Validate the format email (including case sensitive caracteres) with regEx.
 
+
+
+
+# Description
+This repository was created using `Express` framework.  Here we have three basic endpoints to perform some actions to illustrate how Express works.
+
+# Endpoints
+-`GET`  /status   
+-`POST` /calcnumber  
+-`POST` /checkmail
+
+## Requisition examples
+`POST` /calcnumber 
+```
+{
+    "number": 100
+}
+```
+
+`POST` /checkmail 
+```
+{
+    "email": "examplE@gmail.com"
+}
+```
+
+# Installation
+```
+npm install
+```
+
+# Running the app
+## Development
+```
+npm run start:dev
+```
+
+
+
--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { parse } = require('path');
 const app = express();
 const port = 3000;
 app.use(express.json());
@@ -7,33 +6,42 @@ app.use(express.json());
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
-})
+});
+
 
 app.get('/status', (req, res) => {
-  const msg = `Hello!, This app it's working now!`;   
+  const msg = {
+    "status": 200,
+    "message":`Aplication working...`
+  };
   res.send(msg)
-})
+});
 
 app.post('/calcnumber', (req, res) => {
-  
   const reqNumber = req.body.number;
 
   const apiResponse = {
     Number: reqNumber,
     PreviousNumber: (reqNumber - 1),
     NextNumber: (reqNumber + 1)
-  } 
- 
+  }; 
+
   res.send(apiResponse);
+});
 
-})
-
-app.post('/validatemail', (req, res) => {
+app.post('/checkmail', (req, res) => {
   const regEx = new RegExp(/^[\a-z-\.]+@([\w-]+\.)+[\w-]{2,4}$/g);
-  const email = req.body.Email;
+  const email = req.body.email;
   
   if(regEx.test(email)){
-    return res.json(`The email informed is valid!`);
-  }
-  return res.json(`The email informed is invalid. Try again!`);
-})
+    return res.json({
+      "status": 200,
+      "message":`The email informed is valid!`
+    });
+  };
+
+  return res.json({
+    "status": 200,
+    "message":`The email informed is invalid. Please, send a valid email!`
+  });
+});
